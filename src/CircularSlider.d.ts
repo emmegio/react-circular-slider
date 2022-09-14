83e37426dd409d93aaeca29b0f3198b7912e4206
emmegio <emmegio@hotmail.com>
2022-09-14 15:44:36 +0200

    module declaration added

---
+++ src/CircularSlider.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-circular-slider' {
+  import * as CircularSlider from 'react-circular-slider'; // or import Clamscan from "clamscan" or import {Clamscan} from "clamscan" . It depends of how the import is handled
+}
